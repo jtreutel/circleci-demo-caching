1f1460870a17aa912f4e1ecc55e71d7c15708515
Jennings <jtreutel+github@protonmail.com>
2022-03-14 12:24:55 +0900

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Layout from './Components/Layout';
+import Header from './Components/Header';
+import Container from './Components/Container';
+import Card from './Components/Card';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header title="The Aquarium Corner"/>
+        <Container>
+          <Card cards={ this.state.cards } />
+        </Container>
+      </Layout>
+    );
+  }
+
+  state = {
+      cards: []
+  };
+
+  componentDidMount() {
+      fetch('https://www.reddit.com/r/fishpics/top.json?t=all&count=20')
+      .then(res => res.json())
+      .then((data) => {
+          console.log(data.data.children);
+          this.setState({ cards: data.data.children })
+      })
+      .catch(console.log)
+  }
+}
+
+export default App;
